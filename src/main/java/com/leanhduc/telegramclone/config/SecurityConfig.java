d4f5ae9c7a79d8ea5b1f1fdf55ab7da274d5bd3e
leduc <leanhduc140905@gmail.com>
2026-01-21 16:03:45 +0700

    feat(security): add BCryptPasswordEncoder for password hashing and extend UserRepository with username methods

--- src/main/java/com/leanhduc/telegramclone/config/SecurityConfig.java
+++ src/main/java/com/leanhduc/telegramclone/config/SecurityConfig.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -52,6 +53,9 @@ public class SecurityConfig {
         return source;
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
