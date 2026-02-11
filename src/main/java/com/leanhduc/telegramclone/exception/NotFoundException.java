0319f2580525a89f07cee58f7371dc64b4c33b35
leduc <leanhduc140905@gmail.com>
2026-02-11 10:17:35 +0700

    feat(exception): add NotFoundException for handling resource not found errors

---
+++ src/main/java/com/leanhduc/telegramclone/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.leanhduc.telegramclone.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
